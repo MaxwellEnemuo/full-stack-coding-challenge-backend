34f3888a35be96ec21f5d2370d5a0d6c23523bf2
maxwellenemuo <maxwell.enemuo@gmail.com>
2022-12-06 14:49:19 +0100

    enable search on the frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { NextPage } from "next";
 import Link from "next/link";
 
 import Layout from "../components/layout";
-import useApiData from "../hooks/use-api-data";
 import Airport from "../types/airport";
 
+import styles from "../styles/InputField.module.css";
+
 const Page: NextPage = () => {
-  const airports = useApiData<Airport[]>("/api/airports", []);
+  const [value, setValue] = useState("China");
+  const [airports, setAirports] = useState<Airport[]>();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    axios
+      .get(`http://localhost:3001/api/airports/${value}`)
+      .catch((err) => err.response)
+      .then((response) => {
+        setAirports(response.data);
+      });
+  };
 
   return (
     <Layout>
@@ -15,18 +30,36 @@ const Page: NextPage = () => {
       <h2 className="mt-10 text-xl font-semibold">All Airports</h2>
 
       <div>
-        {airports.map((airport) => (
-          <Link
-            className="flex items-center p-5 mt-5 text-gray-800 border border-gray-200 rounded-lg shadow-sm hover:border-blue-600 focus:border-blue-600 focus:ring focus:ring-blue-200 focus:outline-none"
-            href={`/airports/${airport.iata.toLowerCase()}`}
-            key={airport.iata}
-          >
-            <span>
-              {airport.name}, {airport.city}
-            </span>
-            <span className="ml-auto text-gray-500">{airport.country}</span>
-          </Link>
-        ))}
+        <form onSubmit={handleSubmit}>
+          <label>Search:</label>
+          <input
+            className={styles.input}
+            type="text"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            required
+          />
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
+        </form>
+
+        {airports && airports.length > 0
+          ? airports.map((airport) => (
+              <Link
+                className="flex items-center p-5 mt-5 text-gray-800 border border-gray-200 rounded-lg shadow-sm hover:border-blue-600 focus:border-blue-600 focus:ring focus:ring-blue-200 focus:outline-none"
+                href={`/airports/${airport.iata.toLowerCase()}`}
+                key={airport.iata}
+              >
+                <span>
+                  {airport.name}, {airport.city}
+                </span>
+                <span className="ml-auto text-gray-500">{airport.country}</span>
+              </Link>
+            ))
+          : airports}
       </div>
     </Layout>
   );
