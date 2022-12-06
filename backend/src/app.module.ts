67ead1e1261390ee2077f2294ac4472fb7d1412f
maxwellenemuo <maxwell.enemuo@gmail.com>
2022-12-06 14:32:56 +0100

    cleanup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from './prisma/prisma.module';
-import { AirportController } from './airport/airport.controller';
-import { AirportService } from './airport/airport.service';
 import { AirportModule } from './airport/airport.module';
 
 @Module({
